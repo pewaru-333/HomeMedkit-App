a78f09e7ad9dc8592ed29a10d02c42cffb407f83
pav <pewaru@inbox.ru>
2025-01-26 14:16:27 +0300

    === Данное обновление не совместимо с версиями до 1.4.5 включительно! ===
    — Теперь можно более гибко настраивать график приёма лекарств.
    — Для каждого приёма можно задать разную дозу лекарства.
    — Перед назначением приёма в первый раз отображаются необходимы разрешения. Затем данный экран доступен в настройках.
    — Добавлена пункт в настроках, который позволяет исправить работу уведомлений.
    — Исправлено нескольких мелких ошибок.

--- app/src/main/java/ru/application/homemedkit/ui/screens/ScannerScreen.kt
+++ app/src/main/java/ru/application/homemedkit/ui/screens/ScannerScreen.kt
@@ -12,6 +12,7 @@ import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
 import androidx.camera.view.TransformExperimental
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -21,9 +22,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
@@ -43,12 +48,15 @@ import androidx.compose.ui.geometry.RoundRect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.ClipOp
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.clipPath
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.ui.window.Dialog
@@ -58,11 +66,20 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.asExecutor
+import ru.application.homemedkit.R.drawable.vector_barcode
+import ru.application.homemedkit.R.drawable.vector_camera
+import ru.application.homemedkit.R.drawable.vector_check
+import ru.application.homemedkit.R.drawable.vector_datamatrix
 import ru.application.homemedkit.R.drawable.vector_flash
+import ru.application.homemedkit.R.drawable.vector_wrong
 import ru.application.homemedkit.R.string.manual_add
 import ru.application.homemedkit.R.string.text_connection_error
 import ru.application.homemedkit.R.string.text_error_not_medicine
+import ru.application.homemedkit.R.string.text_exit
+import ru.application.homemedkit.R.string.text_explain_camera
+import ru.application.homemedkit.R.string.text_grant
 import ru.application.homemedkit.R.string.text_no
+import ru.application.homemedkit.R.string.text_pay_attention
 import ru.application.homemedkit.R.string.text_permission_grant_full
 import ru.application.homemedkit.R.string.text_request_camera
 import ru.application.homemedkit.R.string.text_try_again
@@ -143,7 +160,7 @@ fun ScannerScreen(navigateUp: () -> Unit, navigateToMedicine: (Long, String, Boo
         }
     }
     else if (cameraPermission.showRationale) PermissionDialog(cameraPermission, navigateUp)
-    else PermissionsScreen(navigateUp, cameraPermission::launchRequest)
+    else FirstTimeScreen(navigateUp, cameraPermission::launchRequest)
 
     when (val data = response) {
         Default -> controller.setImageAnalysisAnalyzer(
@@ -202,6 +219,66 @@ fun Snackbar(id: Int) = Dialog({}, DialogProperties(usePlatformDefaultWidth = fa
         }
     }
 }
+
+@Composable
+private fun FirstTimeScreen(navigateUp: () -> Unit, onGivePermission: () -> Unit) =
+    Column(
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(12.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+        Column(Modifier, Arrangement.spacedBy(8.dp), Alignment.CenterHorizontally) {
+            Image(painterResource(vector_camera), null, Modifier.size(64.dp))
+            Text(
+                text = stringResource(text_pay_attention),
+                style = MaterialTheme.typography.titleLarge
+            )
+            Text(
+                text = stringResource(text_explain_camera),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+        Row(Modifier.fillMaxWidth(), Arrangement.SpaceAround, Alignment.CenterVertically) {
+            Column(Modifier, Arrangement.spacedBy(12.dp), Alignment.CenterHorizontally) {
+                Image(
+                    painter = painterResource(vector_barcode),
+                    contentDescription = null,
+                    modifier = Modifier.size(128.dp),
+                    contentScale = ContentScale.Crop,
+                    colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface)
+                )
+                Image(
+                    painter = painterResource(vector_wrong),
+                    contentDescription = null,
+                    modifier = Modifier.size(64.dp),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Column(Modifier, Arrangement.spacedBy(12.dp), Alignment.CenterHorizontally) {
+                Image(
+                    painter = painterResource(vector_datamatrix),
+                    contentDescription = null,
+                    modifier = Modifier.size(128.dp),
+                    contentScale = ContentScale.Crop,
+                    colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface)
+                )
+                Image(
+                    painter = painterResource(vector_check),
+                    contentDescription = null,
+                    modifier = Modifier.size(64.dp),
+                    contentScale = ContentScale.Crop
+                )
+            }
+        }
+        Row(Modifier.fillMaxWidth(), Arrangement.SpaceBetween, Alignment.CenterVertically) {
+            TextButton(navigateUp) { Text(stringResource(text_exit)) }
+            Button(onGivePermission) { Text(stringResource(text_grant)) }
+        }
+    }
 
 @Composable
 private fun AddMedicineDialog(setDefault: () -> Unit, navigateWithCis: () -> Unit) = AlertDialog(
